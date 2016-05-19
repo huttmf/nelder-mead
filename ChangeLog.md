9695d71db2699b937142ea36ccc12527a850a783
Mike <mfhutt@gmail.com>
2016-05-19 10:10:07 -0400

    Added changelog and license.

---
+++ ChangeLog.md
@@ -0,0 +1,13 @@
+## 2016-05-18
+	* Re-evaluate all vertices after an unsuccessful contraction.
+	* Initial release to github, v1.0
+
+## 2011-03-01
+	* Added constraints.
+
+## 2007-07-23
+	* Fixed memory leak.
+
+## 1999-01-06
+	* Modified to conform to the algorithm presented in Margaret H. Wright's paper on Direct Search Methods.
+
--- nmsimplex.c
+++ nmsimplex.c
@@ -6,42 +6,10 @@
  *
  * An implementation of the Nelder-Mead simplex method.
  *
- * Copyright (c) 1997-present <Michael F. Hutt>
+ * Copyright (c) 1997-present Michael F. Hutt
+ * Released under the MIT License
  *
- * Permission is hereby granted, free of charge, to any person obtaining
- * a copy of this software and associated documentation files (the
- * "Software"), to deal in the Software without restriction, including
- * without limitation the rights to use, copy, modify, merge, publish,
- * distribute, sublicense, and/or sell copies of the Software, and to
- * permit persons to whom the Software is furnished to do so, subject to
- * the following conditions:
- *
- * The above copyright notice and this permission notice shall be
- * included in all copies or substantial portions of the Software.
- *
- * THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND,
- * EXPRESS OR IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF
- * MERCHANTABILITY, FITNESS FOR A PARTICULAR PURPOSE AND
- * NONINFRINGEMENT. IN NO EVENT SHALL THE AUTHORS OR COPYRIGHT HOLDERS BE
- * LIABLE FOR ANY CLAIM, DAMAGES OR OTHER LIABILITY, WHETHER IN AN ACTION
- * OF CONTRACT, TORT OR OTHERWISE, ARISING FROM, OUT OF OR IN CONNECTION
- * WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE SOFTWARE.
- *
- *
- * Jan. 6, 1999 
- * Modified to conform to the algorithm presented
- * in Margaret H. Wright's paper on Direct Search Methods.
- *
- * Jul. 23, 2007
- * Fixed memory leak.
- *
- * Mar. 1, 2011
- * Added constraints.
- *
- * May 18, 2016
- * Re-evaluate all vertices after an unsuccessful contraction.
  */
-
 
 #include "nmsimplex.h"
 
